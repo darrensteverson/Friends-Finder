3597aa126f5c20b619743572e177e303200f9393
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-07-23 17:34:42 -0400

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+    {
+        name: "Darren",
+        image: "",
+        score: [4, 3, 2, 2, 5, 3, 5, 4, 3, 2]
+    },
+    {
+        name: "Noah",
+        image: "",
+        score: [4, 2, 2, 1, 4, 1, 3, 2, 5, 1]
+    },
+    {
+        name: "Donnie",
+        image: "",
+        score: [2, 3, 3, 3, 3, 4, 1, 3, 5, 3]
+    },
+    {
+        name: "Alisha",
+        image: "",
+        score: [5, 3, 5, 1, 3, 5, 5, 3, 5, 5]
+    },
+    {
+        name: "Jimmy",
+        image: "",
+        score: [1, 5, 3, 2, 2, 5, 3, 2, 3, 2]
+    },
+    {
+        name: "Roslyn",
+        image: "",
+        score: [2, 2, 5, 1, 3, 3, 5, 2, 1, 1]
+    },
+    {
+        name: "Ava",
+        image: "",
+        score: [5, 5, 2, 2, 2, 2, 5, 5, 1, 3]
+    },
+
+];
